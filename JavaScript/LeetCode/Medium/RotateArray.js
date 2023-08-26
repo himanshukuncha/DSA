3d55bb61c9389caf05be976285bcd5903782f8f0
Himanshu Kuncha <himanshukuncha@gmail.com>
2023-08-26 08:18:46 +0530

    artay change

--- JavaScript/LeetCode/Medium/RotateArray.js
+++ JavaScript/LeetCode/Medium/RotateArray.js
@@ -1,8 +1,7 @@
 const RotateArray = (nums, k) => {
-    k = k % nums.length;
-    const spliced = nums.splice(nums.length - k, k);
-    nums.unshift(...spliced);
-    console.log(nums);
-}
+  k = k % nums.length;
+  let result = [...nums.slice(-k), ...nums.slice(0, nums.length - k)];
+  console.log(result);
+};
 
 RotateArray([1, 2, 3, 4, 5, 6, 7], 3);
--- RotateArray.js
+++
@@ -1,8 +0,0 @@
-const RotateArray = (nums, k) => {
-    k = k % nums.length;
-    const spliced = nums.splice(nums.length - k, k);
-    nums.unshift(...spliced);
-    console.log(nums);
-}
-
-RotateArray([1, 2, 3, 4, 5, 6, 7], 3);
