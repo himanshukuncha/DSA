98f97de72ccc288b20bd602ade72b80804f51110
Himanshu Kuncha <himanshukuncha@gmail.com>
2023-09-17 16:13:21 +0530

    optimal

--- JavaScript/LeetCode/Medium/RotateArray.js
+++ JavaScript/LeetCode/Medium/RotateArray.js
@@ -1,7 +1,22 @@
-const RotateArray = (nums, k) => {
-  k = k % nums.length;
-  let result = [...nums.slice(-k), ...nums.slice(0, nums.length - k)];
-  console.log(result);
+const reverse = (nums, start, end) => {
+  while (start < end) {
+    [nums[start], nums[end]] = [nums[end], nums[start]];
+    start++;
+    end--;
+  }
 };
 
-RotateArray([1, 2, 3, 4, 5, 6, 7], 3);
+const RotateArray = (nums, k) => {
+  const n = nums.length;
+  k %= n; 
+  reverse(nums, 0, n - 1);
+
+  reverse(nums, 0, k - 1);
+
+  reverse(nums, k, n - 1);
+
+  return nums;
+};
+
+let result = RotateArray([1, 2, 3, 4, 5, 6, 7], 3);
+console.log(result); 
