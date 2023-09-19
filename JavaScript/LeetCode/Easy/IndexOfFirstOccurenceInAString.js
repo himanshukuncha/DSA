b5de44e69467397b667d3847b78f6336fb138996
Himanshu Kuncha <himanshukuncha@gmail.com>
2023-09-19 15:31:34 +0530

    initial

--- JavaScript/LeetCode/Easy/IndexOfFirstOccurenceInAString.js
+++ JavaScript/LeetCode/Easy/IndexOfFirstOccurenceInAString.js
@@ -1,17 +1,15 @@
 const strStr = (hayStack, needle) => {
-  let left = 0
-  let right = needle.length
+  if (needle === "") return 0;
+  if (hayStack.length < needle.length) return -1;
 
-  while (right <= hayStack.length) {
-    if (hayStack.substr(left, right) === needle) {
-      return left
+  for (let i = 0; i <= hayStack.length - needle.length; i++) {
+    if (hayStack.slice(i, i + needle.length) === needle) {
+      return i;
     }
-    left += 1
-    right += 1
   }
 
-  return -1
-}
+  return -1;
+};
 
-console.log(strStr('sadbutsad', 'sad'))
-console.log(strStr('leetcode', 'leeto'))
+console.log(strStr("sadbutsad", "sad"));
+console.log(strStr("leetcode", "leeto"));
