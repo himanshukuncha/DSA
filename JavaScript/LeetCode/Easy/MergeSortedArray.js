d3f0814e3966830ca1d65cc431e62779342c5aa3
Himanshu Kuncha <himanshukuncha@gmail.com>
2023-10-03 10:11:01 +0530

    daily edit

--- JavaScript/LeetCode/Easy/MergeSortedArray.js
+++ JavaScript/LeetCode/Easy/MergeSortedArray.js
@@ -1,11 +1,20 @@
 const mergeSortedArray = (nums1, m, nums2, n) => {
-  for (let i = 0, j = m; i < n; i++) {
-    nums1[j++] = nums2[i]
+  let i = m - 1,
+    j = n - 1,
+    k = m + n - 1;
+  while (i >= 0 && j >= 0) {
+    if (nums1[i] > nums2[j]) {
+      nums1[k--] = nums1[i--];
+    } else {
+      nums1[k--] = nums2[j--];
+    }
   }
 
-  nums1.sort()
+  while (j >= 0) {
+    nums1[k--] = nums2[j--];
+  }
 
-  console.log(nums1)
-}
+  console.log(nums1);
+};
 
-mergeSortedArray([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
+mergeSortedArray([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3);
