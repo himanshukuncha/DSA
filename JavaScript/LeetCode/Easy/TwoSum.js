276bdde4918ac7aa885c349f984288776ce9a167
Himanshu Kuncha <himanshukuncha@gmail.com>
2023-10-07 10:12:22 +0530

    initial commit

--- JavaScript/LeetCode/Easy/TwoSum.js
+++ JavaScript/LeetCode/Easy/TwoSum.js
@@ -1,15 +1,17 @@
 const TwoSum = (nums, target) => {
-  let res = []
-  for (let i = 0; i < nums.length - 1; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
-        res[0] = i
-        res[1] = j
-        break
-      }
-    }
-  }
-  return res
-}
+  const map = {};
 
-console.log(TwoSum([3, 2, 4], 6))
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+
+    if (map[complement] !== undefined) {
+      return [map[complement], i];
+    }
+
+    map[nums[i]] = i;
+  }
+
+  return [];
+};
+
+console.log(TwoSum([3, 2, 4], 6));
