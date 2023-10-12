786bbf2f5bdfb800feb5f5ee8802395b141574ef
Himanshu Kuncha <himanshukuncha@gmail.com>
2023-10-12 08:31:52 +0530

    initial commit

--- JavaScript/LeetCode/Medium/containerWater.js
+++ JavaScript/LeetCode/Medium/containerWater.js
@@ -1,18 +1,22 @@
-const ContainerWithMostWater = (height) => {
+const containerWithMostWater = (height) => {
   let left = 0,
     right = height.length - 1;
-  let max_area = 0;
+  let maxArea = 0;
 
   while (left < right) {
+    const minHeight = Math.min(height[left], height[right]);
+    const width = right - left;
+
+    maxArea = Math.max(maxArea, minHeight * width);
+
     if (height[left] < height[right]) {
-      max_area = Math.max(max_area, height[left] * (right - left));
       left++;
     } else {
-      max_area = Math.max(max_area, height[right] * (right - left));
       right--;
     }
   }
-  return max_area;
+
+  return maxArea;
 };
 
-console.log(ContainerWithMostWater([1, 8, 6, 2, 5, 4, 8, 3, 7]));
+console.log(containerWithMostWater([1, 8, 6, 2, 5, 4, 8, 3, 7]));
