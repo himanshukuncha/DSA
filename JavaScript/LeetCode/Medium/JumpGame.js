6cb33ab3c20c8e552aa3053c18d343c8b9d8c005
Himanshu Kuncha <himanshukuncha@gmail.com>
2023-10-13 08:03:54 +0530

    initial commit

--- JavaScript/LeetCode/Medium/JumpGame.js
+++ JavaScript/LeetCode/Medium/JumpGame.js
@@ -1,15 +1,13 @@
-const JumpGame = (nums) => {
-  if (nums.length == 1 || nums[0] >= nums.length - 1) return true
-  if (nums[0] == 0) return false
+const canJump = (nums) => {
+  let target = nums.length - 1;
 
-  let lastIndex = nums.length - 1
-  for (let i = nums.length - 1; i >= 0; i--) {
-    if (i + nums[i] >= lastIndex) {
-      lastIndex = i
+  for (let i = nums.length - 2; i >= 0; i--) {
+    if (i + nums[i] >= target) {
+      target = i;
     }
   }
 
-  return lastIndex == 0 ? true : false
-}
+  return target === 0;
+};
 
-console.log(JumpGame([2, 3, 1, 1, 4]))
+console.log(canJump([2, 3, 1, 1, 4]));
