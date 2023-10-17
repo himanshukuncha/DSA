ec2d5e602e24e1cdf15d6cf6468900474cf8684d
Himanshu Kuncha <himanshukuncha@gmail.com>
2023-10-17 09:57:11 +0530

    intial commit

--- JavaScript/LeetCode/Medium/TwoSumII.js
+++ JavaScript/LeetCode/Medium/TwoSumII.js
@@ -1,16 +1,12 @@
 const TwoSumII = (nums, target) => {
-    let i = 0;
-    let j = nums.length - 1;
+  let i = 0,
+    j = nums.length - 1;
 
-    while(i < j){
-        if(nums[i] + nums[j] === target){
-            return [i+1, j+1]
-        }
-        else if(nums[i] + nums[j] > target){
-            j--;
-        }
-        else if(nums[i] + nums[j] < target){
-            i++;
-        }
-    }
-}
+  while (i < j) {
+    const sum = nums[i] + nums[j];
+
+    if (sum === target) return [i + 1, j + 1];
+    else if (sum > target) j--;
+    else i++;
+  }
+};
