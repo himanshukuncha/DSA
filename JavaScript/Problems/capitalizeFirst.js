986e48f2063f7c4c7c2e43d6c3d7847fe84a48d3
Himanshu Kuncha <himanshukuncha@gmail.com>
2023-10-25 10:35:05 +0530

    initial commit

--- JavaScript/Problems/capitalizeFirst.js
+++ JavaScript/Problems/capitalizeFirst.js
@@ -1,24 +1,16 @@
-// Given an array of strings, capitalize the first letter of each string using recursion
+function capitalizeFirstWord(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
 function capitalizeFirst(arr) {
-  let result = []
-
-  if (arr.length < 1) return result
-
-  result.push(arr[0][0].toUpperCase() + arr[0].substring(1))
-
-  return result.concat(capitalizeFirst(arr.slice(1)))
+  if (arr.length === 0) return [];
+  return [capitalizeFirstWord(arr[0])].concat(capitalizeFirst(arr.slice(1)));
 }
 
-const capitalizeWords = (arr) => {
-  let result = []
-
-  if (arr.length < 1) return result
-
-  result.push(arr[0].toUpperCase())
-
-  return result.concat(capitalizeWords(arr.slice(1)))
+function capitalizeWords(arr) {
+  if (arr.length === 0) return [];
+  return [arr[0].toUpperCase()].concat(capitalizeWords(arr.slice(1)));
 }
 
-console.log(capitalizeWords(['car', 'taco', 'banana']))
-console.log(capitalizeFirst(['car', 'taco', 'banana']))
+console.log(capitalizeWords(["car", "taco", "banana"]));
+console.log(capitalizeFirst(["car", "taco", "banana"]));
