25c9d08fea470de0a8d71af34dae23b23d076665
Himanshu Kuncha <himanshukuncha@gmail.com>
2023-10-29 14:56:29 +0530

    initial commit

--- JavaScript/Problems/countUniqueValues.js
+++ JavaScript/Problems/countUniqueValues.js
@@ -1,17 +1,13 @@
 const countUniqueValues = (arr) => {
-  let i = 0
+  let i = 0;
 
-  if (arr.length === 0) return 0
+  if (arr.length === 0) return 0;
 
   for (let j = 1; j < arr.length; j++) {
     if (arr[j] !== arr[i]) {
-      i++
-      arr[i] = arr[j]
+      i++;
+      arr[i] = arr[j];
     }
   }
-  return i+1
-}
-
-let a = [2, 2, 2, 1, 5, 6, 12, 0, 14]
-
-console.log(countUniqueValues(a))
+  return i + 1;
+};
