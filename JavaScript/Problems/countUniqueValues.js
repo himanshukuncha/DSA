7b62754c1563568718abcf0baa7d48010bf1087e
Himanshu Kuncha <himanshukuncha@gmail.com>
2023-10-30 19:23:00 +0530

    initial commit

--- JavaScript/Problems/countUniqueValues.js
+++ JavaScript/Problems/countUniqueValues.js
@@ -1,13 +1,28 @@
-const countUniqueValues = (arr) => {
-  let i = 0;
+// const countUniqueValues = (arr) => {
+//   let i = 0;
 
+//   if (arr.length === 0) return 0;
+
+//   for (let j = 1; j < arr.length; j++) {
+//     if (arr[j] !== arr[i]) {
+//       i++;
+//       arr[i] = arr[j];
+//     }
+//   }
+//   return i + 1;
+// };
+
+const countUniqueValues = (arr) => {
   if (arr.length === 0) return 0;
 
-  for (let j = 1; j < arr.length; j++) {
-    if (arr[j] !== arr[i]) {
-      i++;
-      arr[i] = arr[j];
+  let uniquePointer = 0;  // Point to the last found unique value.
+
+  for (let currentPointer = 1; currentPointer < arr.length; currentPointer++) {
+    if (arr[currentPointer] !== arr[uniquePointer]) {
+      uniquePointer++;
+      [arr[uniquePointer], arr[currentPointer]] = [arr[currentPointer], arr[uniquePointer]];
     }
   }
-  return i + 1;
+  
+  return uniquePointer + 1;
 };
