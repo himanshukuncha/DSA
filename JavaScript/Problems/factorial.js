ea3d3750f8e2b3f433b306f0b9528f6cd30f7767
Himanshu Kuncha <himanshukuncha@gmail.com>
2023-11-01 14:33:30 +0530

    initial commit

--- JavaScript/Problems/factorial.js
+++ JavaScript/Problems/factorial.js
@@ -1,7 +1,9 @@
-//recursive approach
 const factorial = (n) => {
-  if (n === 0 || n === 1) return 1
-  else return n * factorial(n - 1)
-}
+  if (n <= 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+};
 
-console.log(factorial(5))
+console.log(factorial(5));
